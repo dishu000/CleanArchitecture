83eeea0b07190b1b8ef11b671348bb272e22761d
Dishu Singla <singladishu2001@gmail.com>
2023-04-14 14:53:16 +0530

    Initial Commit for CleanArch.Mvc Project(Presentation Layer)

---
+++ CleanArch2/CleanArch.mvc/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace CleanArch.mvc.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
